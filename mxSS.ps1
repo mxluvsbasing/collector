34e2788905598aaf506b8175e6d8f48a01f8b7ed
mxluvsbasing <aquaundwiped@gmail.com>
2026-02-26 17:22:56 +0100

    Update mxSS.ps1

--- mxSS.ps1
+++ mxSS.ps1
@@ -2,11 +2,6 @@ Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/DoomsdayFinder.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/SchedulesV2.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1)'
-Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/mxluvsbasing/collector/blob/bc4db255bc514d3686c36509dfc09b0baea15ccd/collector.ps1)'
+Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/mxluvsbasing/collector/main/collector.ps1)'
 Start-Process explorer.exe $env:TEMP
-
 Start-Process explorer.exe 'shell:recent'
-
-
-
-
