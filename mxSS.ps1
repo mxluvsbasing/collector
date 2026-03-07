6e29821b00e115809af0cdbb3db98d13d3d63b11
mxluvsbasing <aquaundwiped@gmail.com>
2026-03-07 18:30:31 +0100

    Update mxSS.ps1

--- mxSS.ps1
+++ mxSS.ps1
@@ -4,6 +4,6 @@ Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/mxluvsbasing/collector/main/collector.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/HadronCollision/PowershellScripts/refs/heads/main/HabibiModAnalyzer.ps1)'
+Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/mxluvsbasing/collector/blob/main/TimelineMasterScript)'
 Start-Process explorer.exe $env:TEMP
 Start-Process explorer.exe 'shell:recent'
-
